406914345503a7618858336bf0f5e6caed5088d4
akkos <a.kosheleva@g.nsu.ru>
2024-03-03 22:09:20 +0700

    add file

--- 9.5.py
+++ 9.5.py
@@ -1,5 +1,8 @@
 n = int(input('Количество кнатов: '))
 sikl = n / 29
 gal = sikl / 17
+sikl_new = abs(int(gal) - gal)*17
+knat_new = abs(int(sikl_new) - sikl_new)*29
 print(f'Количество сиклей: {sikl:.3f}')
 print(f'Количество галлеонов: {gal:.3f}')
+print(f'Стоимость в размере {int(gal)} галлеонов, {int(sikl_new)} сиклей и {int(knat_new)} кнатов')
