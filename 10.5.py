c87e8a23fe89f8321ac53a91789239c091580ae2
akkos <a.kosheleva@g.nsu.ru>
2024-03-16 01:28:52 +0700

    add file

--- 10.5.py
+++ 10.5.py
@@ -3,10 +3,13 @@ f_dgt = a % 10
 s_dgt = a // 10 % 10
 t_dgt = a // 100 % 10
 fo_dgt = a // 1000 % 10
-if a // 1000 < 10 and f_dgt != s_dgt != t_dgt != fo_dgt:
-    if 1900 <= a <= 2050:
-        print('ERROR')
+if a // 1000 < 10:
+    if f_dgt != s_dgt and f_dgt != t_dgt and f_dgt != fo_dgt and s_dgt != t_dgt and s_dgt != fo_dgt and t_dgt != fo_dgt:
+        if 1900 <= a <= 2050:
+            print('OK')
+        else:
+            print('ERROR')
     else:
-        print('OK')
+        print('ERROR')
 else:
     print('ERROR')
